d16ab6e6791aa35d37bf34ffa93ab7234d50c305
Ted Marozzi <edwardmarozzi@gmail.com>
2023-07-20 20:56:01 +1000

    try a base url

--- website/vite.config.ts
+++ website/vite.config.ts
@@ -1,6 +1,10 @@
-import { defineConfig } from 'vite'
-import solid from 'vite-plugin-solid'
+import { defineConfig } from "vite";
+import solid from "vite-plugin-solid";
 
 export default defineConfig({
   plugins: [solid()],
-})
+  base:
+    process.env.NODE_ENV === "production"
+      ? "/programmers-from-the-same-company/"
+      : undefined,
+});
